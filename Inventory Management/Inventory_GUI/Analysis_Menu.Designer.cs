98986ad37240d6d5aa993cc13bf9125c15af0711
trongjhuongwr <nguyentronghuong05@gmail.com>
2024-10-27 17:24:19 +0700

    report

---
+++ Inventory Management/Inventory_GUI/Analysis_Menu.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Inventory_Management.Inventory_GUI
+{
+    partial class Analysis_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Analysis_Menu));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.return_Profile_Click = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart_Analysis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_Supplier_Chart = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.return_Profile_Click)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Analysis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 656);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // return_Profile_Click
+            // 
+            this.return_Profile_Click.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(160)))), ((int)(((byte)(224)))));
+            this.return_Profile_Click.Image = ((System.Drawing.Image)(resources.GetObject("return_Profile_Click.Image")));
+            this.return_Profile_Click.Location = new System.Drawing.Point(0, 300);
+            this.return_Profile_Click.Name = "return_Profile_Click";
+            this.return_Profile_Click.Size = new System.Drawing.Size(64, 50);
+            this.return_Profile_Click.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.return_Profile_Click.TabIndex = 11;
+            this.return_Profile_Click.TabStop = false;
+            this.return_Profile_Click.Click += new System.EventHandler(this.return_Profile_Click_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.panel1.Controls.Add(this.chart_Analysis);
+            this.panel1.Controls.Add(this.button_Supplier_Chart);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(80, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1336, 624);
+            this.panel1.TabIndex = 17;
+            // 
+            // chart_Analysis
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart_Analysis.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart_Analysis.Legends.Add(legend6);
+            this.chart_Analysis.Location = new System.Drawing.Point(24, 78);
+            this.chart_Analysis.Name = "chart_Analysis";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart_Analysis.Series.Add(series6);
+            this.chart_Analysis.Size = new System.Drawing.Size(1288, 530);
+            this.chart_Analysis.TabIndex = 9;
+            this.chart_Analysis.Text = "Analysis Chart";
+            // 
+            // button_Supplier_Chart
+            // 
+            this.button_Supplier_Chart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(72)))), ((int)(((byte)(101)))));
+            this.button_Supplier_Chart.Font = new System.Drawing.Font("Roboto", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Supplier_Chart.ForeColor = System.Drawing.Color.White;
+            this.button_Supplier_Chart.Location = new System.Drawing.Point(1072, 8);
+            this.button_Supplier_Chart.Name = "button_Supplier_Chart";
+            this.button_Supplier_Chart.Size = new System.Drawing.Size(240, 48);
+            this.button_Supplier_Chart.TabIndex = 4;
+            this.button_Supplier_Chart.TabStop = false;
+            this.button_Supplier_Chart.Text = "Supplier";
+            this.button_Supplier_Chart.UseVisualStyleBackColor = false;
+            this.button_Supplier_Chart.Click += new System.EventHandler(this.button_Supplier_Chart_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(72)))), ((int)(((byte)(101)))));
+            this.panel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(19)))), ((int)(((byte)(1)))));
+            this.panel3.Location = new System.Drawing.Point(24, 64);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1288, 8);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Righteous", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(72)))), ((int)(((byte)(101)))));
+            this.label1.Location = new System.Drawing.Point(24, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Analysis Report";
+            // 
+            // Analysis_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1432, 653);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.return_Profile_Click);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Analysis_Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TrackerX";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.return_Profile_Click)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Analysis)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox return_Profile_Click;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Supplier_Chart;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_Analysis;
+    }
+}
